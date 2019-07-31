74a9b3a56de2fcc74e6a60bfa897f985ee7b3c90
Maximilian Pal <max.pal@outlook.com>
2019-07-31 09:38:20 +0200

    Initializ Space Magic, and GUI.py

--- SpaceMagic.py
+++ SpaceMagic.py
@@ -1,26 +1,23 @@
 import re
 
+col_1 = 0
+col_2 = 0
+
+
 def get_table_from_file(filename):
     with open(filename, "r+") as myfile:
         table = [line.strip().split(",") for line in myfile]
 
     return table
 
-csv_1 = "Personal info.csv"
-#csv_1 = input("Please, add base CSV file name: ")
-col_1 = "0"
 
-#csv_2 = input("Please, add base CSV file name: ")
-col_2 = "0"
-csv_2 = "DrGregoryHouse.csv"
-
-def get_rendered_tajs(filename):
-    table = get_table_from_file(filename)
+def get_rendered_tajs(table):
     TAJ = []
     [TAJ.append(re.sub("-", "", element[col_1])) for element in table]
     new_tajs = [re.sub(" ", "", tajs) for tajs in TAJ]
     taj_dict = {tajnumber: index for index, tajnumber in enumerate(new_tajs)}
     return taj_dict
+
 
 def compare(lista1, lista2): #lista = list of rendered tajnumbers
     common_tajs = []
@@ -31,20 +28,10 @@ def compare(lista1, lista2): #lista = list of rendered tajnumbers
     # return dict(map(lambda kv: {kv[0], (kv[1], lista2.get(kv[0], None))}, lista1.items()))
 
 
-def main():
-    csv_1 = "Personal info.csv"
-    csv_2 = "DrGregoryHouse.csv"
-    table_1 = get_table_from_file(csv_1)
-    table_2 = get_table_from_file(csv_2)
-    newtajs_1 = get_rendered_tajs(csv_1)
-    newtajs_2 = get_rendered_tajs(csv_2)
+def main(csv1, csv2):
+    table_1 = get_table_from_file(csv1)
+    table_2 = get_table_from_file(csv2)
+    newtajs_1 = get_rendered_tajs(table_1)
+    newtajs_2 = get_rendered_tajs(table_2)
     common_tajs = compare(newtajs_1, newtajs_2)
-    print(newtajs_1)
-    print(newtajs_2)
     print(common_tajs)
-
-
-
-main()
-
-
---
+++ gui.py
@@ -0,0 +1,72 @@
+import SpaceMagic
+import tkinter
+from tkinter.filedialog import askopenfilename
+
+
+##main
+##background
+root = tkinter.Tk()
+root.wm_iconbitmap('diagrams.ico')
+root.iconbitmap('diagrams.ico')
+root.title("Merge No More!")
+root.configure(background="white")
+root.geometry("730x165")
+
+##file browsers
+def import_csv1_data():
+    global csv1
+    csv_file_path = askopenfilename()
+    print(csv_file_path)
+    pathlabel1.config(text=csv_file_path)
+    csv1 = (csv_file_path)
+
+def import_csv2_data():
+    global csv2
+    csv_file_path = askopenfilename()
+    print(csv_file_path)
+    pathlabel2.config(text=csv_file_path)
+    csv2 = (csv_file_path)
+
+
+#Browse button 1 +label
+csv1 = ""
+browsebutton1 = tkinter.Button(root, text="Add Base Spreadsheet",  width=50, command=import_csv1_data)
+browsebutton1.pack()
+pathlabel1 = tkinter.Label(root)
+pathlabel1.pack()
+
+#Colum selector button
+# columselectorbutton1 = tkinter.Button(root, text="Select index of colum", width=50, command=)
+
+#Browse button 2 +label
+csv2 = ""
+browsebutton2 = tkinter.Button(root, text="Add Comparison Spreadsheet", width=50, command=import_csv2_data)
+browsebutton2.pack()
+pathlabel2 = tkinter.Label(root)
+pathlabel2.pack()
+
+#Merge function call:
+def merge(event):
+    SpaceMagic.main(csv1, csv2)
+
+
+#Merge button
+mergebutton = tkinter.Button(root, text="Merge", width=50)
+mergebutton.bind("<Button-1>", merge)
+mergebutton.pack()
+
+
+##exit function
+def close_root():
+    root.destroy()
+    exit()
+
+
+# #exit button
+exitbutton = tkinter.Button(root, text="Exit", width=50, command=close_root)
+exitbutton.pack()
+
+
+
+#run the main loop
+root.mainloop()
